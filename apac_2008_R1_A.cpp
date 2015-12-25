751d2a35c4aac631d99d0eceba5821227b4d75c8
AbhishekTiwari0812 <abhishektiwari08121994007@gmail.com>
2015-12-25 13:34:13 +0530

    sort both, reverse 2nd and correlate!

--- apac_2008_R1_A.cpp
+++ apac_2008_R1_A.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <algorithm>
 #include <map>
+#include <algorithm>
 #include <set>
 #include <queue>
 #include <cmath>
@@ -21,11 +21,27 @@ int main(){
 	out.open("output.txt");
 	int test;
 	in>>test;
+	int size;
+	lli *v1;
+	lli *v2;
 	for(int i0=0;i0<test;i0++){
-
-
-		cout<<"Case #"<<i0+1<<": "<< /*****VALUE HERE***/<<endl;		
-	}	
+		in>>size;
+		v1=new lli[size];
+		v2=new lli[size];
+		for(int i=0;i<size;i++){
+			in>>v1[i];
+		}
+		for(int i=0;i<size;i++){
+			in>>v2[i];
+		}
+		sort(v1,v1+size);
+		sort(v2,v2+size);
+		long long int mul=0;
+		for(int i=0;i<size;i++){
+			mul+=v1[i]*v2[size-i-1];
+		}
+		out<<"Case #"<<i0+1<<": "<< mul<<endl;
+	}
 
 	in.close();
 	out.close();
